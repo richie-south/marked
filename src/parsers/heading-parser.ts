4d9debe239da74ae0e4ae1620fdf4773f2c47eed
richie-south <richard.soderman@gmail.com>
2024-10-31 20:45:45 +0100

    feat: heading parser

--- src/parsers/heading-parser.ts
+++ src/parsers/heading-parser.ts
@@ -1,13 +1,12 @@
 import {createElement} from '..'
 import {Parser} from '../type'
 
-export const headingParser: Parser<'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'> = ({
-  parseElements,
-}) => {
+type Headings = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
+export const headingParser: Parser<Headings> = ({parseElements}) => {
   return {
     regex: /^(#{1,6}) (.+)$/gm,
     replacer: (id, _, hashes: string, content: string) => {
-      const type = `h${hashes.length}` as 'h1'
+      const type = `h${hashes.length}` as Headings
 
       return createElement(type, parseElements(content), id)
     },
